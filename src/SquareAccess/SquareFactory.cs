8a074ec900f0e496275fc9f15ff55ba1472b7fa2
Maxim S <maxim.strelsov@skuvault.com>
2019-09-06 15:01:27 -0400

    Add CreateOrdersService() placeholder

--- src/SquareAccess/SquareFactory.cs
+++ src/SquareAccess/SquareFactory.cs
@@ -1,7 +1,14 @@
-﻿namespace SquareAccess
+﻿using SquareAccess.Configuration;
+using SquareAccess.Services.Orders;
+using SquareAccess.Throttling;
+
+namespace SquareAccess
 {
     public class SquareFactory : ISquareFactory
     {
-        
+	    public ISquareOrdersService CreateOrdersService(SquareConfig config, Throttler throttler)
+	    {
+		    throw new System.NotImplementedException();
+	    }
     }
 }
